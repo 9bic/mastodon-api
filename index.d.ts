fe21a3d42b502e4191afa689990e0ed30f0d056a
saga <xatm092.saga@gmail.com>
2018-08-17 10:22:21 +0900

    index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,35 +1,34 @@
-export default Mastodon;
-export as namespace Mastodon;
-
-export interface MastodonConfig {
+declare interface MastodonConfig {
   access_token: String 
   timeout_ms?: String
   api_url?: String
 }
-export interface GetParams {
+declare interface GetParams {
 
 }
 
+declare type MastodonApiCallback = (err, data, response) => void
 
-export type MastodonApiCallback = (err, data, response) => void
+declare module "mastodon-api" {
+  class Mastodon {
+    constructor(config: MastodonConfig)
+    static createOAuthApp(url?: String, clientName?: String, scopes?: String, redirectUri?: String, webSite?: String): Promise<any>
 
-export class Mastodon {
-  constructor(config: MastodonConfig)
-  static createOAuthApp(url?: String, clientName?: String, scopes?: String, redirectUri?: String, webSite?: String): Promise<any>
+    static getAuthorizationUrl(clientId: String, clientSecret: String, baseUrl?: String, scope?: String, redirectUri?: String): Promise<string>
+    static getAccessToken(clientId: String, clientSecret: String, authorizationCode: String, baseUrl?: String)
 
-  static getAuthorizationUrl(clientId: String, clientSecret: String, baseUrl?: String, scope?: String, redirectUri?: String): Promise<string>
-  static getAccessToken(clientId: String, clientSecret: String, authorizationCode: String, baseUrl?: String)
+    get(path: String, params: GetParams, callback: MastodonApiCallback)
+    get(path: String, callback: MastodonApiCallback)
 
-  get(path: String, params: GetParams, callback: MastodonApiCallback)
-  get(path: String, callback: MastodonApiCallback)
+    post(path: String, params: GetParams, callback: MastodonApiCallback)
+    post(path: String, callback: MastodonApiCallback)
 
-  post(path: String, params: GetParams, callback: MastodonApiCallback)
-  post(path: String, callback: MastodonApiCallback)
+    stream(path: String, params: GetParams)
+    stream(path: String)
 
-  stream(path: String, params: GetParams)
-  stream(path: String)
-
-  getAuth()
-  setAuth(tokens: String)
-  static formEncodeParams(params, noQuestionMark)
+    getAuth()
+    setAuth(tokens: String)
+    static formEncodeParams(params, noQuestionMark)
+  }
+  export = Mastodon
 }
